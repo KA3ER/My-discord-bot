49c52e85c679f7cdb437b2a6ecda955cd8b80b67
KA3ER <82054335+KA3ER@users.noreply.github.com>
2022-05-01 18:19:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const brnclient = require("./structures/brnClient")
-new brnclient().start("ODM4ODQwMDc0MTQxMDQwNjcx.YJA8sg.NtZnzP7S3wUzEoHxOx9dVM23bjQ", "./brmcommands") //Brncray1 Token
+new brnclient().start("YOUR TOKEN HERE", "./brmcommands") //Brncray1 Token
